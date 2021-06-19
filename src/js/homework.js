1f52fde7e2485e545c08688919c5722fd827698e
d3frag-coder <84383696+d3frag-coder@users.noreply.github.com>
2021-06-19 13:32:37 +0500

    Add files via upload

--- src/js/homework.js
+++ src/js/homework.js
@@ -1,28 +1,4 @@
-let one = prompt("Какой Вы хотите сайт? 0 - Визитка, 1 - Корпоративный, 2 - Интернет-магазин");
-let two = prompt("На чем адаптивность? 0 - Только под мобилки, 1 - Только под ПК, 2 - Full");
-let three = prompt("Вы дизайн предоставите? 0 - Да, 1 - Нет, делай сам =)");
-let price = 0;
-
-if(one == "0") {
-    price = 30000;
-} else if(one == "1") {
-    price = 35000;
-} else {
-    price = 180000;
-}
-
-if(one == "0") {
-    price += 5000;
-} else if(one == "1") {
-    price += 5000;
-} else {
-    price += 15000;
-}
-
-if(one == "0") {
-    price += 0;
-} else {
-    price += 8000;
-}
-
-alert("Стоимость сайта: " + price);
+$('a[href^="#"]').click(function(){
+    let valHref = $(this).attr("href");
+    $('html, body').animate({scrollTop: $(valHref).offset().top + "px"});
+});

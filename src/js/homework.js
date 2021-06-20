470628dbb2756c9ad2ae2cb30fbb5b8a53989506
d3frag-coder <84383696+d3frag-coder@users.noreply.github.com>
2021-06-20 10:15:41 +0500

    Add files via upload

--- src/js/homework.js
+++ src/js/homework.js
@@ -1,3 +1,13 @@
+setTimeout(function(){
+    $.colorbox({
+        html: "<h5>Стой! И ты получишь скидку!</h5>",
+        className: "cta",
+        width: 350,
+        height: 150
+    });
+}, 30000);
+
+
 $('a[href^="#"]').click(function(){
     let valHref = $(this).attr("href");
     $('html, body').animate({scrollTop: $(valHref).offset().top + "px"});
